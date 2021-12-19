a1cd53679b3932250f8199a83dff3ccec5066473
Muhammad Ahsan <muhammad.ahsan9776@gmail.com>
2021-12-20 01:57:04 +0500

    Created DBHelper

---
+++ app/src/main/java/com/example/mc_databaseprac/DBHelper.java
@@ -0,0 +1,67 @@
+package com.example.mc_databaseprac;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String STUDENT_NAME = "StudentName";
+    public static final String STUDENT_AGE = "StudentAge";
+    public static final String STUDENT_ACTIVE = "IsStudentActive";
+    public static final String STUDENT_ID = "StudentID";
+    public static final String STUDENT_TABLE = "StudentTable";
+
+    public DBHelper(@Nullable Context context) {
+        super(context, "Student.db", null, 4);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String query =
+                "CREATE TABLE " + STUDENT_TABLE +
+                        "( " + STUDENT_ID + " Integer PRIMARY KEY AUTOINCREMENT," + STUDENT_NAME + " Text," +
+                        STUDENT_AGE + " Integer, " + STUDENT_ACTIVE + " BOOL)";
+        sqLiteDatabase.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + STUDENT_TABLE);
+        onCreate(sqLiteDatabase);
+    }
+
+    void addStudent(StudentModel student) {
+        ContentValues cv = new ContentValues();
+
+        cv.put(STUDENT_NAME, student.getName().toString());
+        cv.put(STUDENT_AGE, Integer.toString(student.getAge()));
+        cv.put(STUDENT_ACTIVE, student.isActive());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(STUDENT_TABLE, null, cv);
+        db.close();
+
+    }
+
+    ArrayList<StudentModel> getStudents() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM " + STUDENT_TABLE, null);
+
+        ArrayList<StudentModel> students = new ArrayList<>();
+
+        if (cursor.moveToFirst()) {
+            do {
+                students.add(new StudentModel(cursor.getString(1), cursor.getInt(2), cursor.getInt(3) == 0 ? false : true));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        db.close();
+        return students;
+    }
+}
